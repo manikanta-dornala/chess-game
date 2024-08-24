f28536610da330597fb9ad7808bb0e0f09a65aae
Manikanta Dornala <manikanta.dornala@outlook.com>
2024-08-24 03:29:26 -0700

    cleanup

--- src/common/game.tsx
+++ src/common/game.tsx
@@ -19,28 +19,24 @@ export class GameState {
     validSquares: Set<string> = new Set();
     moves: Array<IMove> = [];
     boards: Array<{ [position: string]: IPiece | null }> = [];
+
     constructor() {
-        let initialPositions = initial_piece_positions;
-        this.board = {};
-        for (let j = 0; j < BoardCoordinateSystem.ranks.length; j++) {
-            for (let i = 0; i < BoardCoordinateSystem.files.length; i++) {
-                let position =
-                    BoardCoordinateSystem.files[i] +
-                    BoardCoordinateSystem.ranks[j];
-                this.board[position] = initialPositions[position]
-                    ? initialPositions[position]
-                    : null;
+        this.board = { ...initial_piece_positions };
+        BoardCoordinateSystem.ranks.forEach((rank) =>
+            BoardCoordinateSystem.files.forEach((file) => {
+                const position = `${file}${rank}`;
                 this.validSquares.add(position);
-            }
-        }
+                if (!this.board[position]) {
+                    this.board[position] = null;
+                }
+            })
+        );
     }
 
     takeBack() {
         if (this.boards.length > 0) {
             this.moves.pop();
-            const lastboard = this.boards.pop();
-            if (lastboard) this.board = lastboard;
-
+            this.board = this.boards.pop() || this.board;
             this.turn =
                 this.turn === ChessColor.Light
                     ? ChessColor.Dark
@@ -50,20 +46,18 @@ export class GameState {
 
     makeMove(curr: string, target: string) {
         const currPiece = this.board[curr];
-        if (!currPiece) return; // no piece to move
+        if (!currPiece) return;
 
-        // check if there's any move that can land curr piece in target
-        const moves = this.getPossibleMoves(currPiece, curr).filter(
+        const validMove = this.getPossibleMoves(currPiece, curr).find(
             (move) => move.target === target
         );
 
-        if (moves.length) {
-            this.boards.push(structuredClone(this.board));
-            const move = moves[0];
+        if (validMove) {
+            this.boards.push({ ...this.board });
             this.board[target] = currPiece;
             this.board[curr] = null;
 
-            if (move.type === MoveType.EnPassant) {
+            if (validMove.type === MoveType.EnPassant) {
                 const lastMove = this.lastMove();
                 if (lastMove) this.board[lastMove.target] = null;
             }
@@ -72,202 +66,175 @@ export class GameState {
                 this.turn === ChessColor.Light
                     ? ChessColor.Dark
                     : ChessColor.Light;
-            this.moves.push(move);
+            this.moves.push(validMove);
         }
     }
 
     getPossibleTargets(piece: IPiece, position: string): Array<string> {
-        return this.getPossibleMoves(piece, position).map((m) => m.target);
+        return this.getPossibleMoves(piece, position).map(
+            (move) => move.target
+        );
     }
 
     getPossibleMoves(piece: IPiece, position: string): Array<IMove> {
         if (this.turn !== piece.color) return [];
-        if (piece.name === PieceName.Pawn) {
-            return this.getPawnMoves(piece, position);
+
+        switch (piece.name) {
+            case PieceName.Pawn:
+                return this.getPawnMoves(piece, position);
+            case PieceName.Rook:
+            case PieceName.Bishop:
+            case PieceName.Queen:
+            case PieceName.King:
+            case PieceName.Knight:
+                return this.getPieceMoves(piece, position);
+            default:
+                return [];
         }
-        if (
-            [
-                PieceName.Rook,
-                PieceName.Bishop,
-                PieceName.Queen,
-                PieceName.King,
-                PieceName.Knight,
-            ].includes(piece.name)
-        ) {
-            return this.getPieceMoves(piece, position);
-        }
-        return [];
     }
 
     getPawnMoves(piece: IPiece, position: string): Array<IMove> {
-        const file = position[0];
-        const rank = parseInt(position[1]);
+        const [file, rank] = [position[0], parseInt(position[1])];
         const moves: IMove[] = [];
-
-        const moveDirection =
-            this.direction[this.turn] === ChessDirection.Up ? 1 : -1;
+        const moveDir =
+            this.direction[piece.color] === ChessDirection.Up ? 1 : -1;
         const baseRank =
-            this.direction[this.turn] === ChessDirection.Up ? 2 : 7;
+            this.direction[piece.color] === ChessDirection.Up ? 2 : 7;
         const enPassantRank =
-            this.direction[this.turn] === ChessDirection.Up ? 5 : 4;
+            this.direction[piece.color] === ChessDirection.Up ? 5 : 4;
 
-        const forwardOne = `${file}${rank + moveDirection}`;
-        const forwardTwo = `${file}${rank + 2 * moveDirection}`;
-        const captureLeft = `${String.fromCharCode(file.charCodeAt(0) - 1)}${rank + moveDirection}`;
-        const captureRight = `${String.fromCharCode(file.charCodeAt(0) + 1)}${rank + moveDirection}`;
+        const addMove = (target: string, type: MoveType) =>
+            moves.push({ piece, type, target, position });
 
-        // One step forward
+        const forwardOne = `${file}${rank + moveDir}`;
+        const forwardTwo = `${file}${rank + 2 * moveDir}`;
+        const captureTargets = [
+            `${String.fromCharCode(file.charCodeAt(0) - 1)}${rank + moveDir}`,
+            `${String.fromCharCode(file.charCodeAt(0) + 1)}${rank + moveDir}`,
+        ];
+
+        // Move forward
         if (this.validSquares.has(forwardOne) && !this.board[forwardOne]) {
-            moves.push({
-                piece,
-                type: MoveType.Move,
-                target: forwardOne,
-                position,
-            });
-
-            // Two steps forward from the base rank
+            addMove(forwardOne, MoveType.Move);
             if (
                 rank === baseRank &&
                 this.validSquares.has(forwardTwo) &&
                 !this.board[forwardTwo]
             ) {
-                moves.push({
-                    piece,
-                    type: MoveType.PawnMove2,
-                    target: forwardTwo,
-                    position,
-                });
+                addMove(forwardTwo, MoveType.PawnMove2);
             }
         }
 
-        // Captures (only if an enemy piece is present)
-        [captureLeft, captureRight].forEach((target) => {
+        // Capture and En Passant
+        captureTargets.forEach((target) => {
             if (this.validSquares.has(target)) {
                 const targetPiece = this.board[target];
-                if (targetPiece && targetPiece.color !== this.turn) {
-                    moves.push({
-                        piece,
-                        type: MoveType.Capture,
-                        target,
-                        position,
-                    });
+                if (targetPiece && targetPiece.color !== piece.color) {
+                    addMove(target, MoveType.Capture);
+                } else if (rank === enPassantRank && !targetPiece) {
+                    const enPassantPawnPos = `${target[0]}${rank}`;
+                    const lastMove = this.lastMove();
+                    if (
+                        lastMove?.type === MoveType.PawnMove2 &&
+                        lastMove.target === enPassantPawnPos
+                    ) {
+                        addMove(target, MoveType.EnPassant);
+                    }
                 }
             }
         });
 
-        // En passant
-        if (rank === enPassantRank) {
-            [captureLeft, captureRight].forEach((target) => {
-                const enPassantPawnPos = `${target[0]}${rank}`;
-                const lastMove = this.lastMove();
-                if (
-                    this.validSquares.has(target) &&
-                    !this.board[target] &&
-                    lastMove?.type === MoveType.PawnMove2 &&
-                    lastMove.target === enPassantPawnPos
-                ) {
-                    moves.push({
-                        piece,
-                        type: MoveType.EnPassant,
-                        target,
-                        position,
-                    });
-                }
-            });
-        }
-
         return moves;
     }
 
-    getPieceMoves(piece: IPiece, position: string) {
-        const file = position[0];
-        const rank = parseInt(position[1]);
+    getPieceMoves(piece: IPiece, position: string): Array<IMove> {
+        const [file, rank] = [position[0], parseInt(position[1])];
         const moves: IMove[] = [];
-
-        const rook_move_set = [
-            { file: 0, rank: 1 }, // up
-            { file: 0, rank: -1 }, // down
-            { file: -1, rank: 0 }, // left
-            { file: 1, rank: 0 }, // right
-        ];
-
-        const bishop_move_set = [
-            { file: 1, rank: 1 }, // up-right
-            { file: 1, rank: -1 }, // down-right
-            { file: -1, rank: 1 }, // up-left
-            { file: -1, rank: -1 }, // down-left
-        ];
-
-        const queen_king_move_set = [...rook_move_set, ...bishop_move_set];
-
-        const knight_move_set = [
-            { file: 2, rank: 1 },
-            { file: 2, rank: -1 },
-            { file: -2, rank: 1 },
-            { file: -2, rank: -1 },
-            { file: 1, rank: 2 },
-            { file: 1, rank: -2 },
-            { file: -1, rank: 2 },
-            { file: -1, rank: -2 },
-        ];
-
-        let move_set: Array<{
+        interface IMoveSet {
             file: number;
             rank: number;
-        }> = [];
-        let move_count = 1;
-        if (piece.name === PieceName.Bishop) {
-            move_set = bishop_move_set;
-            move_count = 8;
         }
-        if (piece.name === PieceName.Rook) {
-            move_set = rook_move_set;
-            move_count = 8;
-        }
-        if (piece.name === PieceName.Queen) {
-            move_set = queen_king_move_set;
-            move_count = 8;
-        }
-        if (piece.name === PieceName.King) {
-            move_set = queen_king_move_set;
-            move_count = 2;
-        }
-        if (piece.name === PieceName.Knight) {
-            move_set = knight_move_set;
-            move_count = 2;
-        }
+        const moveSets: {
+            [key in PieceName]: Array<IMoveSet>;
+        } = {
+            [PieceName.Rook]: [
+                { file: 0, rank: 1 },
+                { file: 0, rank: -1 },
+                { file: 1, rank: 0 },
+                { file: -1, rank: 0 },
+            ],
+            [PieceName.Bishop]: [
+                { file: 1, rank: 1 },
+                { file: 1, rank: -1 },
+                { file: -1, rank: 1 },
+                { file: -1, rank: -1 },
+            ],
+            [PieceName.Queen]: [
+                { file: 0, rank: 1 },
+                { file: 0, rank: -1 },
+                { file: 1, rank: 0 },
+                { file: -1, rank: 0 },
+                { file: 1, rank: 1 },
+                { file: 1, rank: -1 },
+                { file: -1, rank: 1 },
+                { file: -1, rank: -1 },
+            ],
+            [PieceName.King]: [
+                { file: 0, rank: 1 },
+                { file: 0, rank: -1 },
+                { file: 1, rank: 0 },
+                { file: -1, rank: 0 },
+                { file: 1, rank: 1 },
+                { file: 1, rank: -1 },
+                { file: -1, rank: 1 },
+                { file: -1, rank: -1 },
+            ],
+            [PieceName.Knight]: [
+                { file: 2, rank: 1 },
+                { file: 2, rank: -1 },
+                { file: -2, rank: 1 },
+                { file: -2, rank: -1 },
+                { file: 1, rank: 2 },
+                { file: 1, rank: -2 },
+                { file: -1, rank: 2 },
+                { file: -1, rank: -2 },
+            ],
+            [PieceName.Pawn]: [],
+        };
 
-        for (const { file: df, rank: dr } of move_set) {
-            for (let i = 1; i < move_count; i++) {
+        const moveLimit =
+            piece.name === PieceName.King || piece.name === PieceName.Knight
+                ? 1
+                : 8;
+        const moveSet = moveSets[piece.name];
+
+        for (const { file: df, rank: dr } of moveSet) {
+            for (let i = 1; i <= moveLimit; i++) {
                 const targetFile = String.fromCharCode(
                     file.charCodeAt(0) + df * i
                 );
                 const targetRank = rank + dr * i;
                 const targetPos = `${targetFile}${targetRank}`;
 
-                // Check if the target position is a valid square
                 if (!this.validSquares.has(targetPos)) break;
 
                 const targetPiece = this.board[targetPos];
                 if (targetPiece) {
-                    // Stop if a piece is encountered
                     if (targetPiece.color !== piece.color) {
-                        // Capture the enemy piece
                         moves.push({
-                            piece: piece,
+                            piece,
                             type: MoveType.Capture,
                             target: targetPos,
-                            position: position,
+                            position,
                         });
                     }
-                    break; // Stop further movement in this direction
+                    break;
                 } else {
-                    // If the square is empty, add the move
                     moves.push({
-                        piece: piece,
+                        piece,
                         type: MoveType.Move,
                         target: targetPos,
-                        position: position,
+                        position,
                     });
                 }
             }
@@ -276,9 +243,7 @@ export class GameState {
         return moves;
     }
 
-    lastMove() {
-        if (this.moves.length) {
-            return this.moves[this.moves.length - 1];
-        }
+    lastMove(): IMove | undefined {
+        return this.moves[this.moves.length - 1];
     }
 }
