f2c4be67c7a01a217583020af67c87cdc96ddee2
Manikanta Dornala <manikanta.dornala@outlook.com>
2024-08-31 12:02:27 -0700

    make App a class

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,45 @@ import './App.css';
 import ChessComponent from './components/chess/chess';
 import { ChessColor } from './common/enums';
 import { RandomBot } from './common/bots/random-bot';
-function App() {
-    const [resetKey, setResetKey] = useState(0);
+import Select, {
+    components,
+    ControlProps,
+    Props,
+    StylesConfig,
+} from 'react-select';
 
-    const handleReset = () => {
-        setResetKey((prevKey) => prevKey + 1); // Change the key to force a component restart
+class App extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            resetKey: 0,
+        };
+    }
+
+    handleReset = () => {
+        this.setState((prevState: any) => ({
+            resetKey: prevState.resetKey + 1, // Change the key to force a component restart
+        }));
     };
-    return (
-        <div className="Chess">
-            <button onClick={handleReset}>New Game</button>
-            <hr></hr>
-            <ChessComponent
-                bot={new RandomBot(ChessColor.Dark)}
-                bottomColor={ChessColor.Light}
-                id={resetKey}
-            ></ChessComponent>
-        </div>
-    );
+
+    render() {
+        const { resetKey } = this.state;
+
+        return (
+            <div className="Chess">
+                <span>
+                    <button onClick={this.handleReset}>New Game</button>
+                </span>
+
+                <hr />
+                <ChessComponent
+                    bot={new RandomBot(ChessColor.Dark)}
+                    bottomColor={ChessColor.Light}
+                    id={resetKey}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
